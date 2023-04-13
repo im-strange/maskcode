2d3108a7e42accbc5900433424a47a8ed8e01f31
im-strange <itsmestrange.dump@gmail.com>
2023-04-13 09:18:50 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='maskcode',
+    version='0.1',
+    url='https://github.com/im-strange/maskcode',
+    author='Samuel Genoguin',
+    packages=['maskcode'],
+    install_requires=[],
+)
